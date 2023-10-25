c959ecc2877e14c6b15d37058f6cd46f3593e461
vatsalxbyte <vatsal.zinzuvadiya@xbyte.io>
2023-10-25 11:45:19 +0530

    first commit

---
+++ app/(routes)/home/page.js
@@ -0,0 +1,14 @@
+import Header from '@/app/components/header/header';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <div className='pt-[44px] md:pt-[127px] nxl:pt-[80px]'>
+           <Header/>
+            </div>
+        </>
+    )
+}
+
+export default Home
---
+++ app/(routes)/innerpage/page.js
@@ -0,0 +1,13 @@
+import Header from '@/app/components/header/header';
+import React from 'react';
+
+const Inner = () => {
+    return (
+        <>
+            <div className='pt-[44px] md:pt-[127px] nxl:pt-[80px]'>
+         <><h1>INNER PAGE</h1></>
+            </div>
+        </>
+    )
+}
+export default Inner
